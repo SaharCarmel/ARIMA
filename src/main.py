8190ad931718899ed5fec1d30df91518c9a80742
SaharCarmel <saharsela271@gmail.com>
2022-01-17 16:34:19 +0200

    Fixed a bug i inference How: I accidently input to the forward the test data and not the infered data

--- src/main.py
+++ src/main.py
@@ -5,38 +5,31 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 
-# datamodel = ARIMA(p=0, d=1, q=1)
-# sampleSize = 20
-trainSize = 14
-# sampleData = datamodel.generateSample(sampleSize)
-
-
+trainSize = 140
 sampleData = torch.tensor(np.load('data.npy'))
 sampleSize = len(sampleData)
 trainData = sampleData[:trainSize]
 
 predictionModel = ARIMA(p=0, d=1, q=1)
-predictionModel.fit(trainData, epochs=10, learningRate=0.1)
-print(predictionModel.dWeights)
-print(predictionModel.qWeights)
+predictionModel.fit(trainData, epochs=100, learningRate=0.01)
 
 testData = sampleData[trainSize:]
 inference = torch.zeros(sampleSize)
 errors = torch.tensor(np.random.normal(
     loc=0, scale=1, size=sampleSize), dtype=torch.float32)
 with torch.no_grad():
-    for i in range(sampleSize - 2):
+    for i in range(len(testData) - 2):
         inference[i+2] = predictionModel.forward(
-            testData[0:i+2], errors[0:i+2])
+            inference[0:i+2], errors[0:i+2])
 
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=torch.arange(sampleSize), y=sampleData,
                          mode='lines',
                          name='sampleData'))
-fig.add_trace(go.Scatter(x=torch.arange(trainSize), y=inference[0:trainSize].detach().numpy(),
-                         mode='lines+markers',
-                         name='overfit'))
-fig.add_trace(go.Scatter(x=torch.arange(len(inference[trainSize:]))+trainSize, y=inference[trainSize:].detach().numpy(),
+# fig.add_trace(go.Scatter(x=torch.arange(trainSize), y=inference[0:trainSize].detach().numpy(),
+#                          mode='lines+markers',
+#                          name='overfit'))
+fig.add_trace(go.Scatter(x=torch.arange(len(testData))+trainSize, y=inference.detach().numpy(),
                          mode='lines+markers',
                          name='predicted'))
 fig.show()
