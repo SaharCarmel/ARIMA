58875b2ac4d662ac63c98e2c2df2511b510d5708
SaharCarmel <saharsela271@gmail.com>
2022-01-17 16:49:09 +0200

    Modified create data to be 20 length

--- src/create_data.py
+++ src/create_data.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 datamodel = ARIMA(p=0, d=1, q=1)
-sampleSize = 200
+sampleSize = 20
 sampleData = datamodel.generateSample(sampleSize)
 np.save('data.npy', sampleData.detach().numpy())
 
