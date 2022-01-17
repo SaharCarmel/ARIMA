e96bfaa77e0bf40e03d8295f7f678ab4eb9207a4
SaharCarmel <saharsela271@gmail.com>
2022-01-17 11:31:07 +0200

    Created the basic AR Model

---
+++ src/arima_model.py
@@ -0,0 +1,24 @@
+import torch
+class ARIMA(torch.nn.Module):
+    """ARIMA [summary]
+    """
+
+    def __init__(self,
+                 p: int = 0,
+                 d: int = 0,
+                 q: int = 0) -> None:
+        """__init__ [summary]
+
+        Args:
+            p (int): The number of lag observations included in the model,
+                    also called the lag order.
+            d (int): The number of times that the raw observations are
+                    differenced, also called the degree of differencing.
+            q (int): The size of the moving average window,
+                    also called the order of moving average.
+        """
+        super(ARIMA, self).__init__()
+        self.p = p
+        self.pWeights = torch.rand(p+1)
+        self.pWeights.requires_grad = True
+        pass
