9f557f40ea741563ff05915877f5a6febffcf864
SaharCarmel <saharsela271@gmail.com>
2022-01-17 20:42:37 +0200

    Added basic tests

--- tests/test_example.py
+++ tests/test_example.py
@@ -5,7 +5,12 @@ from src.arima_model import ARIMA
 class TestArima(unittest.TestCase):
     def test_init(self):
         model = ARIMA(p=1, d=0, q=0)
-        self.assertEqual(len(model.pWeights), 2)
+        self.assertEqual(len(model.pWeights), 1)
+
+    def test_fit(self):
+        testModel = ARIMA(p=0, d=1, q=1)
+        sampleData = testModel.generateSample(200)
+        fitModel = testModel.fit(sampleData, epochs=100, learningRate=0.01)
 
 
 if __name__ == '__main__':
